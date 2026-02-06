471824641118391fe33df02683411960cd2dc126
Mairaj <mairajk929@gmail.com>
2026-02-06 19:54:16 +0530

    Added Pages

---
+++ client/src/pages/BrandDash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BrandDash = () => {
+  return (
+    <div>BrandDash</div>
+  )
+}
+
+export default BrandDash
---
+++ client/src/pages/InfluencerDash.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InfluencerDash = () => {
+  return (
+    <div>InfluencerDash</div>
+  )
+}
+
+export default InfluencerDash
