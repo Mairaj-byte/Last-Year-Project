a04e393bd4b7b543a5d9aeb2ee895a14ccfee22c
Mairaj <mairajk929@gmail.com>
2026-03-10 23:22:49 +0530

    Campaings in Dashboard

--- client/src/context/ShopContext.jsx
+++ client/src/context/ShopContext.jsx
@@ -1,22 +1,38 @@
 import { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
 const ShopContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+
   const [token, setToken] = useState(localStorage.getItem("token"));
   const [identity, setIdentity] = useState(localStorage.getItem("identity"));
+  const [brandId, setBrandId] = useState(localStorage.getItem("brandId"));
 
   const logout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("identity");
+    localStorage.removeItem("brandId");
+
     setToken(null);
     setIdentity(null);
-    navigate("/")
+    setBrandId(null);
+
+    navigate("/");
   };
 
   return (
     <ShopContext.Provider
-      value={{ token, setToken, identity, setIdentity, logout }}
+      value={{
+        token,
+        setToken,
+        identity,
+        setIdentity,
+        brandId,
+        setBrandId,
+        logout,
+      }}
     >
       {children}
     </ShopContext.Provider>
